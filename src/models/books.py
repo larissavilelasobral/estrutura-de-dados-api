c3aeaa2ec4c5d2ebd7a4f9ccaa37ca967f6e76c9
larissavilelasobral <larissavilelasobral@gmail.com>
2023-01-26 14:03:55 -0300

    a

--- src/models/books.py
+++ src/models/books.py
@@ -1,59 +1,7 @@
 import uuid
-  
+from repository.books_repository import livros  
+
 class Livro:
-  
-  livros = [
-  {
-      'id': 1,
-      'título': 'Código limpo: Habilidades práticas do Agile Software',
-      'autor': 'Robert C. Martin'
-  },
-  {
-      'id': 2,
-      'título': 'Arquitetura limpa: O guia para estrutura de software',
-      'autor': 'Robert C. Martin'
-  },
-  {
-      'id': 3,
-      'título': 'Microsserviços Prontos Para a Produção',
-      'autor': 'Susan J. Fowler'
-  },
-  {
-      'id': 4,
-      'título': 'Domain-Driven Design: Atacando as complexidades do software',
-      'autor': 'Eric Evans'
-  },
-  {
-      'id': 5,
-      'título': 'Refatoração: Aperfeiçoando o Design de Códigos',
-      'autor': 'Martin Fowler'
-  },
-  {
-      'id': 6,
-      'título': 'O Programador Pragmático: De Aprendiz a Mestre',
-      'autor': 'Andrew Hunt & David Thomas'
-  },
-  {
-      'id': 7,
-      'título': 'Entendendo Algoritmos: Um Guia Ilustrado Para Programadores',
-      'autor': 'Aditya Y. Bhargava'
-  },
-  {
-      'id': 8,
-      'título': 'Estruturas de Dados e Algoritmos com JavaScript',
-      'autor': 'Loiane Groner'
-  },
-  {
-      'id': 9,
-      'título': 'Aprendendo TypeScript: Melhore Suas Habilidades',
-      'autor': 'Josh Goldberg'
-  },
-  {
-      'id': 10,
-      'título': 'Linux Eficiente na Linha de Comando',
-      'autor': 'Daniel J. Barrett'
-  },
-]
   
   def __init__(self, título, autor):
     # atributos do livro
@@ -64,7 +12,7 @@ class Livro:
 
 # metodos para obter e atualizar informações sobre um livro especifico ou para adicionar e remover livros da lista
   def obter_livro(id):
-    for livro in Livro.livros:
+    for livro in livros:
         if livro.id == id:
             return livro
     return None
